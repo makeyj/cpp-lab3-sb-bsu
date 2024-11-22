49c01dbd9c6fb59731fde1f52049a2e872a9b49a
makeyj <vlad.makeychik2006@gmail.com>
2024-11-22 12:31:36 +0300

    Add files via upload

---
+++ 3a-t4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int k, i = 1, sum
+k = 0, number;
+_
+cout << " Enter the amount of integers to sum = "; cin >> k;
+do {
+cout << "Enter integer nr.
+" << i << ": ";
+cin >> number;
+sum
+k += number; i++;
+_
+} while (i <= k);
+cout << " The total sum of " << k << " integers is: " << sum
+return 0;
+k;
+_
+}
