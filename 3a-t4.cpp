4e129435b520931a3ce56322dcac0d4e1c6972d5
makeyj <vlad.makeychik2006@gmail.com>
2024-11-22 12:54:12 +0300

    Add files via upload

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,20 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include <limits>
+
+
 using namespace std;
 int main()
+
 {
 int k, i = 1, sum_k = 0, number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
+
 if (k==0) {
     cout << "No integers can be summed";
+    return 0;
 }
 
-else {
+ 
 do {
 cout << "Enter integer nr. " << i << ": ";
-cin >> number;
+if (cin >> number) {
 sum_k += number; i++;
+}
+
+
+    else {
+cout << "You've entered not int number." << endl;
+ cin.clear();
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }  
+
+
 } while (i <= k);
 cout << " The total sum of " << k << " integers is: " << sum_k;
-}
+
+
+
 return 0;
 }
